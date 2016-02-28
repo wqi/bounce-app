de6c0a412d9487d46b19d6b70e32f90474900a51
William Qi <wqi@lai.io>
2016-02-27 16:17:49 -0800

    abstract cardview to component

--- app/iOS/index.js
+++ app/iOS/index.js
@@ -7,52 +7,13 @@ import React, {
   View
 } from "react-native";
 
-import Swiper from "react-native-swiper";
-
-// Styles
-const styles = StyleSheet.create({
-  wrapper: {
-  },
-  slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold',
-  }
-})
+import CardView from "../Shared/Home/CardView.js"
 
 // Classes
 export default class Bounce extends Component {
   render() {
     return (
-      <Swiper style={styles.wrapper} showsButtons={true}>
-        <View style={styles.slide1}>
-          <Text style={styles.text}>Hello Swiper</Text>
-        </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
-        </View>
-      </Swiper>
+      <CardView/>
     )    
   }
 } 
